90ab1e021e5cc7097a3a62a0256a0fc4daf6ba4e
Louise Fok <fokl@sheridanc.on.ca>
2011-12-01 20:52:13 -0500

    Modify noop-iosched to close lfok/unixproject#2

--- proj01-iosched.c
+++ proj01-iosched.c
@@ -1,5 +1,9 @@
 /*
- * elevator noop
+ * elevator proj01
+ * 
+ * Team 06
+ * Louise Fok
+ * ID: 991183076
  */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
@@ -11,13 +15,13 @@ struct noop_data {
 	struct list_head queue;
 };
 
-static void noop_merged_requests(struct request_queue *q, struct request *rq,
+static void proj01_merged_requests(struct request_queue *q, struct request *rq,
 				 struct request *next)
 {
 	list_del_init(&next->queuelist);
 }
 
-static int noop_dispatch(struct request_queue *q, int force)
+static int proj01_dispatch(struct request_queue *q, int force)
 {
 	struct noop_data *nd = q->elevator->elevator_data;
 
@@ -31,14 +35,14 @@ static int noop_dispatch(struct request_queue *q, int force)
 	return 0;
 }
 
-static void noop_add_request(struct request_queue *q, struct request *rq)
+static void proj01_add_request(struct request_queue *q, struct request *rq)
 {
 	struct noop_data *nd = q->elevator->elevator_data;
 
 	list_add_tail(&rq->queuelist, &nd->queue);
 }
 
-static int noop_queue_empty(struct request_queue *q)
+static int proj01_queue_empty(struct request_queue *q)
 {
 	struct noop_data *nd = q->elevator->elevator_data;
 
@@ -46,7 +50,7 @@ static int noop_queue_empty(struct request_queue *q)
 }
 
 static struct request *
-noop_former_request(struct request_queue *q, struct request *rq)
+proj01_former_request(struct request_queue *q, struct request *rq)
 {
 	struct noop_data *nd = q->elevator->elevator_data;
 
@@ -56,7 +60,7 @@ noop_former_request(struct request_queue *q, struct request *rq)
 }
 
 static struct request *
-noop_latter_request(struct request_queue *q, struct request *rq)
+proj01_latter_request(struct request_queue *q, struct request *rq)
 {
 	struct noop_data *nd = q->elevator->elevator_data;
 
@@ -65,7 +69,7 @@ noop_latter_request(struct request_queue *q, struct request *rq)
 	return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static void *noop_init_queue(struct request_queue *q)
+static void *proj01_init_queue(struct request_queue *q)
 {
 	struct noop_data *nd;
 
@@ -76,7 +80,7 @@ static void *noop_init_queue(struct request_queue *q)
 	return nd;
 }
 
-static void noop_exit_queue(elevator_t *e)
+static void proj01_exit_queue(elevator_t *e)
 {
 	struct noop_data *nd = e->elevator_data;
 
@@ -84,37 +88,37 @@ static void noop_exit_queue(elevator_t *e)
 	kfree(nd);
 }
 
-static struct elevator_type elevator_noop = {
+static struct elevator_type elevator_proj01 = {
 	.ops = {
-		.elevator_merge_req_fn		= noop_merged_requests,
-		.elevator_dispatch_fn		= noop_dispatch,
-		.elevator_add_req_fn		= noop_add_request,
-		.elevator_queue_empty_fn	= noop_queue_empty,
-		.elevator_former_req_fn		= noop_former_request,
-		.elevator_latter_req_fn		= noop_latter_request,
-		.elevator_init_fn		= noop_init_queue,
-		.elevator_exit_fn		= noop_exit_queue,
+		.elevator_merge_req_fn		= proj01_merged_requests,
+		.elevator_dispatch_fn		= proj01_dispatch,
+		.elevator_add_req_fn		= proj01_add_request,
+		.elevator_queue_empty_fn	= proj01_queue_empty,
+		.elevator_former_req_fn		= proj01_former_request,
+		.elevator_latter_req_fn		= proj01_latter_request,
+		.elevator_init_fn		= proj01_init_queue,
+		.elevator_exit_fn		= proj01_exit_queue,
 	},
-	.elevator_name = "noop",
+	.elevator_name = "team06",
 	.elevator_owner = THIS_MODULE,
 };
 
-static int __init noop_init(void)
+static int __init proj01_init(void)
 {
-	elv_register(&elevator_noop);
+	elv_register(&elevator_proj01);
 
 	return 0;
 }
 
-static void __exit noop_exit(void)
+static void __exit proj01_exit(void)
 {
-	elv_unregister(&elevator_noop);
+	elv_unregister(&elevator_proj01);
 }
 
-module_init(noop_init);
-module_exit(noop_exit);
+module_init(proj01_init);
+module_exit(proj01_exit);
 
 
-MODULE_AUTHOR("Jens Axboe");
+MODULE_AUTHOR("Louise Fok");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("No-op IO scheduler");
+MODULE_DESCRIPTION("Proj01 No-op IO scheduler");
